1b176bed822f6b4fc460d5aa18ddab7199974936
REdo_o <edo-o@mail.ru>
2021-03-26 16:27:06 +0400

    fixed double order bug in case of slow network

--- app/src/main/java/com/pos/bringit/network/Network.java
+++ app/src/main/java/com/pos/bringit/network/Network.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.NetworkResponse;
 import com.android.volley.NoConnectionError;
 import com.android.volley.ParseError;
@@ -254,6 +255,10 @@ public class Network {
                         return params;
                     }
                 };
+        jsonArrayRequest.setRetryPolicy(new DefaultRetryPolicy(
+                DefaultRetryPolicy.DEFAULT_TIMEOUT_MS * 2,
+                0,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         RequestQueueSingleton.getInstance(context).addToRequestQueue(jsonArrayRequest);
     }
 
@@ -375,6 +380,10 @@ public class Network {
                 return params;
             }
         };
+        req.setRetryPolicy(new DefaultRetryPolicy(
+                DefaultRetryPolicy.DEFAULT_TIMEOUT_MS * 2,
+                0,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         RequestQueueSingleton.getInstance(context).addToRequestQueue(req);
     }
 
